f56a975c564a447696fb41cf1c87911f40ba2e7b
FedeCasper <federicorouyere@gmail.com>
2023-10-20 16:17:33 -0300

    Second checkpoint complete

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,20 +1,21 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 
-export const ItemCount = ({numberOfItems, fnAddQuantity, fnSubtractQuantity}) => {
+export const ItemCount = ({numberOfItems, fnAddQuantity, fnSubtractQuantity, numberOfStock}) => {
 
 
    return (
-      <div>
-            <div className=' flex flex-col justify-between items-center gap-4 bg-slate-200 p-2 rounded-md'>
+      <div className='flex flex-col items-center gap-2'>
+            <div className='w-full flex flex-col justify-between items-center gap-4 bg-slate-200 p-2 rounded-md'>
                <h3>Camisa Tiger</h3>
+               <h5>Available stock: {numberOfStock}</h5>
                <div className=' flex justify-between w-full'>
                   <AddCircleIcon onClick={fnAddQuantity} />
                   {numberOfItems}
                   <RemoveCircleIcon onClick={fnSubtractQuantity} />
                </div>
             </div>
-            <button className=" bg-slate-400 p-2 rounded-md">Agregar al carrito</button>
+            <button className="w-full transition-all ease-in-out bg-pink-100 rounded p-2 drop-shadow-md hover:bg-lime-300">Agregar al carrito</button>
       </div>
    )
 }
--- src/components/ItemListContainer.js/ItemListContainer.jsx
+++ src/components/ItemListContainer.js/ItemListContainer.jsx
@@ -1,23 +1,35 @@
 import { ItemCount } from "../ItemCount/ItemCount"
 import { useState } from "react"
 
-export const ItemListContainer = ( {greeting} ) => {
+export const ItemListContainer = ( { greeting } ) => {
 
    const [quantity, setQuantity] = useState(0)
+   const [stock, setStock] = useState(10)
 
    function addQuantity(){
-      setQuantity(quantity + 1)
+      if( quantity < (stock + quantity) ){
+         setQuantity(quantity + 1)
+         setStock(stock - 1)
+      }else{
+         alert("No hay stock disponible")
+      }
    }
 
    function subtractQuantity(){
-      setQuantity(quantity - 1)
+      if( quantity > 0 ){
+         setQuantity(quantity - 1)
+         setStock(stock + 1)
+      }else{
+         alert("Ya no tiene elementos agregados")
+      }
    }
 
    return (
       <section className=" flex flex-col gap-3 ">
          <h2 className=" w-fit text-2xl font-sansSerif text-gray-600 "> {greeting} </h2>
          <ItemCount 
-         numberOfItems={quantity}  
+         numberOfItems={quantity}
+         numberOfStock={stock}
          fnAddQuantity={addQuantity}
          fnSubtractQuantity={subtractQuantity} />
       </section>
