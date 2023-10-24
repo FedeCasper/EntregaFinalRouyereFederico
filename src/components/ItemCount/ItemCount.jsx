8512942092e8079fecbeeac05c0bd47e122750d0
FedeCasper <federicorouyere@gmail.com>
2023-10-23 22:16:37 -0300

    Corrections

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,18 +1,40 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import { useRef, useState } from "react"
 
-export const ItemCount = ({numberOfItems, fnAddQuantity, fnSubtractQuantity, numberOfStock}) => {
+export const ItemCount = () => {
 
+   const [quantity, setQuantity] = useState(0)
+   // const [stock, setStock] = useState(10)
+   let refStock = useRef(10)
+
+   function addQuantity(){
+      if( quantity < (refStock.current + quantity) ){
+         setQuantity(quantity + 1)
+         refStock.current --
+      }else{
+         alert("No hay stock disponible")
+      }
+   }
+
+   function subtractQuantity(){
+      if( quantity > 0 ){
+         setQuantity(quantity - 1)
+         refStock.current ++
+      }else{
+         alert("Ya no tiene elementos agregados")
+      }
+   }
 
    return (
       <div className='flex flex-col items-center gap-2'>
             <div className='w-full flex flex-col justify-between items-center gap-4 bg-slate-200 p-2 rounded-md'>
                <h3>Camisa Tiger</h3>
-               <h5>Available stock: {numberOfStock}</h5>
+               <h5>Available stock: {refStock.current}</h5>
                <div className=' flex justify-between w-full'>
-                  <RemoveCircleIcon onClick={fnSubtractQuantity} className=' cursor-pointer  hover:text-lime-600' />
-                  {numberOfItems}
-                  <AddCircleIcon onClick={fnAddQuantity} className=' cursor-pointer hover:text-lime-600'/>
+                  <RemoveCircleIcon onClick={subtractQuantity} className=' cursor-pointer hover:text-lime-600' />
+                  {quantity}
+                  <AddCircleIcon onClick={addQuantity} className=' cursor-pointer hover:text-lime-600'/>
                </div>
             </div>
             <button className="w-full transition-all ease-in-out bg-pink-100 rounded p-2 drop-shadow-md hover:bg-lime-300">Agregar al carrito</button>
--- src/components/ItemListContainer.js/ItemListContainer.jsx
+++ src/components/ItemListContainer.js/ItemListContainer.jsx
@@ -1,37 +1,11 @@
 import { ItemCount } from "../ItemCount/ItemCount"
-import { useState } from "react"
 
 export const ItemListContainer = ( { greeting } ) => {
-
-   const [quantity, setQuantity] = useState(0)
-   const [stock, setStock] = useState(10)
-
-   function addQuantity(){
-      if( quantity < (stock + quantity) ){
-         setQuantity(quantity + 1)
-         setStock(stock - 1)
-      }else{
-         alert("No hay stock disponible")
-      }
-   }
-
-   function subtractQuantity(){
-      if( quantity > 0 ){
-         setQuantity(quantity - 1)
-         setStock(stock + 1)
-      }else{
-         alert("Ya no tiene elementos agregados")
-      }
-   }
 
    return (
       <section className=" flex flex-col gap-3 ">
          <h2 className=" w-fit text-2xl font-sansSerif text-gray-600 "> {greeting} </h2>
-         <ItemCount 
-         numberOfItems={quantity}
-         numberOfStock={stock}
-         fnAddQuantity={addQuantity}
-         fnSubtractQuantity={subtractQuantity} />
+         <ItemCount />
       </section>
    )
 }
