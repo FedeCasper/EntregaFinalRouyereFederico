a5111cd42cf7b5a5c7db0556aad4db2d626cb3f7
FedeCasper <federicorouyere@gmail.com>
2023-10-25 16:32:29 -0300

    Various things

--- src/components/Item/item.jsx
+++ src/components/Item/item.jsx
@@ -1,11 +1,12 @@
-const Item = ({product, brand, color, image, component}) => {
+const Item = ({product, brand, color, image, component, description}) => {
 
    return (
       <div className=" bg-slate-300 rounded-md h-min-48 w-48 p-2 cursor-pointer shadow-md">
          <img src={image} alt="" className="h-32 w-full object-cover mb-2"/>
          <h2 className=" font-bold first-letter:uppercase mb-2">{product}</h2>
-         <p>{brand}</p>
-         <h3>{color}</h3>
+         <p className=" italic text-sm mb-2">{description}</p>
+         <p className="font-medium first-letter:uppercase">{brand}</p>
+         <h3 className=" font-medium first-letter:uppercase">{color}</h3>
          {component}
       </div>
    )
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,34 +2,38 @@ import Item from '../Item/Item.jsx'
 import { useEffect, useState } from "react" 
 import PulseLoader from "react-spinners/PulseLoader";
 
-
 const PRODUCTS = [
    {
       product: "keyboard",
+      description: "A high-quality mechanical keyboard for a great typing experience.",
       color: "black",
       brand: "logitech",
       image: "https://candid.technology/wp-content/uploads/2020/12/msi-lights-feature-2048x1147.jpg"
    },
    {
       product: "mouse",
-      color: "white",
+      description: "An ergonomic and responsive optical mouse for precise control.",
+      color: "lime",
       brand: "razer",
       image: "https://th.bing.com/th/id/OIP.u7OWDE1yIr7W0YSneOQS3gHaE8?pid=ImgDet&rs=1"
    },
    {
       product: "monitor",
+      description: "A 55-inch 4K Ultra HD Smart TV with built-in streaming apps.",
       color: "black",
       brand: "dell",
       image: "https://th.bing.com/th/id/OIP.oYM3lQAsy9FyCajnHNabgQHaE8?pid=ImgDet&rs=1"
    },
    {
       product: "headphones",
-      color: "white",
+      description: "Over-ear headphones with noise-canceling technology for immersive sound.",
+      color: "red",
       brand: "sony",
       image: "https://insta-gaming.nl/wp-content/uploads/2020/10/dragon3-768x768.jpg"
    },
    {
       product: "laptop",
+      description: "A powerful laptop with the latest processor and dedicated graphics card.",
       color: "purple",
       brand: "dell",
       image: "https://th.bing.com/th/id/R.1c40043af725b05a534a512e614a2322?rik=CZmjUkndapyRDA&riu=http%3a%2f%2fcdn1.theinertia.com%2fwp-content%2fuploads%2f2017%2f06%2fDell-XPS-13-2-in-1-Header-900-1200x694.jpg&ehk=tWHDlzpThnxQfyWqDAYyixhxIDiOgdkcvjod8DSWdtY%3d&risl=&pid=ImgRaw&r=0"
@@ -50,7 +54,7 @@ const ItemList = (props) => {
          setTimeout( () => {
             setLoader(false)
             resolve(PRODUCTS)
-         }, 3000)
+         }, 2000)
       })
       .then( (res) => setProductList(res) )
       .catch( (err) => console.log(err) )
@@ -64,6 +68,7 @@ const ItemList = (props) => {
             <Item 
                key={`${index}`} 
                product={product.product} 
+               description={product.description}
                color={product.color} 
                brand={product.brand} 
                image={product.image} 
