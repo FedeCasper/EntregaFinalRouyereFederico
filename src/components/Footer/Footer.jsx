e56d52037e3b217cfcfef168e7f7819a72f54596
FedeCasper <federicorouyere@gmail.com>
2023-12-06 22:38:16 -0300

    Final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,11 @@ const Footer = () => {
                   <h1 className="max-w-lg text-xl font-semibold tracking-tight xl:text-2xl ">Subscribe our newsletter to get update.</h1>
 
                   <div class="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
-                     <input id="email" type="text" class="px-4 py-2 bg-white border rounded-md dark:border-gray-600 focus:border-[#30E0A1] focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Email Address" />
+                     <input disabled={true} id="email" type="text" class="px-4 py-2 bg-white border rounded-md dark:border-gray-600 focus:border-[#30E0A1] focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Email Address" />
 
-                        <button class="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+                        <button 
+                        disabled={true}
+                        class="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
                            Subscribe
                         </button>
                   </div>
